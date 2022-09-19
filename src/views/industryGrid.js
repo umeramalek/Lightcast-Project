3f0b7c6a3874a3624011b5d4940fc63850c4b024
umeramalek <umeramalek796@gmail.com>
2022-09-18 22:19:31 -0700

    completed industries employing computer programmers grid

--- src/views/industryGrid.js
+++ src/views/industryGrid.js
@@ -35,3 +35,37 @@ class IndustryGrid extends Component {
         }
         return rows;
     } 
+
+    // creates a new array called rowData - which loops thorugh the jobs in the data and creates an object for each job and adds that object to the rowData array
+    dataRow(rowData, jobs){
+        // calculate by dividing the number of occupation jobs by the number of total jobs
+        const occPer = (rowData.in_occupation_jobs/jobs*100).toFixed(1);
+        // calculate by dividing number of occupation jobs by row data jobs
+        const totPer = (rowData.in_occupation_jobs/rowData.jobs*100).toFixed(1);
+        // // grid of cells, each containing a title and percentage of people in the occupation
+        const backgroundColor = 'linear-gradient(to right, #0096ff17 '+(occPer*2)+'%, white 0%)';
+        return (<>
+        {/* create columns with styling to produce the resulted data */}
+            <Col lg={7} className="gridCol colorBlue" style={{background: backgroundColor}}><span>@ {rowData.title}</span></Col>
+            <Col className="gridCol rightAlign"><span>{formatNumber(rowData.in_occupation_jobs)}</span></Col>
+            <Col className="gridCol rightAlign"><span>{occPer}%</span></Col>
+            <Col className="gridCol rightAlign"><span>{totPer}%</span></Col>
+        </>);
+    }
+
+
+    // the function returns an array of objects containing information about companies that employ computer programmers.
+    render(){
+        return(
+        <div style={{paddingTop:'30px'}}>
+            <h5>Industries Employing Computer Programmers</h5>
+            <hr className="headerLine"></hr>
+            <Container fluid className="grid">
+                {this.industriesGrid()}
+            </Container>
+        </div>
+        )
+    }   
+}
+
+export default IndustryGrid;
