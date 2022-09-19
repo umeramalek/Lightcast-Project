eefccf2e7c35332b59ff829dce734bf43181f0ff
umeramalek <umeramalek796@gmail.com>
2022-09-19 01:17:43 -0700

    assign a constructor and pass a prop, and fetch the data and set its state

--- src/views/regionGraph.js
+++ src/views/regionGraph.js
@@ -3,3 +3,26 @@ import CanvasJSReact from '../assets/canvasjs.react';
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
+class RegionGraph extends Component {
+	// constructor function of the component will be passed in the prop argument and it will set the state property with an empty object
+	constructor(prop){
+		super(prop);
+		this.state ={
+			regionGraphData: null
+		}
+	}
+
+	// fetchs the data using then method and set it as state
+	fetchData() {
+		fetch('mock-data2.json'	// include server api path
+			, {
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json'
+				}
+			}).then(resp => resp.json()).then((result) => {
+				this.setState({
+					regionGraphData : result
+				});
+			});
+	}
