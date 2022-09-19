6bbbb143b0e96e5b43ad074cc21f303820f90627
umeramalek <umeramalek796@gmail.com>
2022-09-18 21:19:19 -0700

    industry grid - iterated through the industry data and styled it

--- src/views/industryGrid.js
+++ src/views/industryGrid.js
@@ -14,3 +14,24 @@ class IndustryGrid extends Component {
             {this.dataRowLoop()}
         </>); 
     }
+
+    // header row - creates each column for the given title and year and style it
+    // font - 7, style it to align right,  
+    headerRow(year){
+        return (<>
+            <Col lg={7} className="gridCol"><span>Industry</span></Col>
+            <Col className="gridCol rightAlign"><span>Occupation Jobs in Industry ({year})</span></Col>
+            <Col className="gridCol rightAlign"><span>% of Occupation in Industry ({year})</span></Col>
+            <Col className="gridCol rightAlign"><span>% of Total Jobs in Industry ({year})</span></Col>
+        </>);
+    }
+
+     // dataRowLoop - using  for loop iterates through the industries data and creates a row for each industry
+    
+     dataRowLoop(){
+        const rows=[];
+        for(let i=0;i< this.props.industriesData.industries.length;i++){
+            rows.push(<Row key={i} className="gridRow">{ this.dataRow(this.props.industriesData.industries[i], this.props.industriesData.jobs) } </Row>)
+        }
+        return rows;
+    } 
