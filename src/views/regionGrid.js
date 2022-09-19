b83e05a203f7ef079e2f394e439c1a5697b15e73
umeramalek <umeramalek796@gmail.com>
2022-09-18 22:37:37 -0700

    region grid header and rows setup

---
+++ src/views/regionGrid.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import {formatNumber} from './utils';
+
+class RegionGrid extends Component {
+
+    // declaring a variable - which is an objetc that will be used to create the grid 
+    regionalGrid() {
+        {/* 4 rows will be created */}
+        return (<>
+        {/* 1st row contains the header information for each region  */}
+            <Row className="gridHeaderRow">{ this.headerRow() } </Row>
+            {/*3 rows for regional, state and nation with shapes   */}
+            <Row className="gridRow">{ this.dataRow(this.props.regionalData.regional, 'Region', 'circle') } </Row>
+            <Row className="gridRow">{ this.dataRow(this.props.regionalData.state, 'State', 'square') } </Row>
+            <Row className="gridRow">{ this.dataRow(this.props.regionalData.nation, 'Nation', 'triangle') } </Row>
+        </>); 
+    }
+
+
+    // header row styling - each column styling with job year and perentage changed
+    headerRow(){
+        return (<>
+            <Col className="gridCol" style={{maxWidth: '50px'}}><span></span></Col>
+            <Col lg={7} className="gridCol"><span>Region</span></Col>
+            <Col className="gridCol rightAlign"><span>{this.props.regionalData.start_year} Jobs</span></Col>
+            <Col className="gridCol rightAlign"><span>{this.props.regionalData.end_year} Jobs</span></Col>
+            <Col className="gridCol rightAlign"><span>Change</span></Col>
+            <Col className="gridCol rightAlign"><span>% Change</span></Col>
+        </>);
+    }
+
